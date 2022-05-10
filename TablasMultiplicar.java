9c1ac77f562aa45eb2c6c39847d14d06cac1940e
Oscar Machasilla Martínez <105305043+Jason-182@users.noreply.github.com>
2022-05-10 18:25:08 +0200

    Add files via upload

---
+++ TablasMultiplicar.java
@@ -0,0 +1,13 @@
+
+	public class TablasMultiplicar{
+	
+		public static void main(String arg[]){
+		
+			for(int i=1;i<=10; i++){
+				System.out.println("**** tabla de multiplicar del "+i+" ****");
+				for(int j=1;j<=10;j++){
+					System.out.println("* "+i+ " x "+ j + " = "+ (j*i));
+				}
+			}
+		}
+	}
